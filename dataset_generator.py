f111dd00383da1defab3cd6f79cf5d9b0cf775e9
MohammadKachuee <m.kachuee@gmail.com>
2018-01-15 15:43:35 -0800

    Hapt database added.

--- dataset_generator.py
+++ dataset_generator.py
@@ -5,15 +5,15 @@
 
 
 
-available_datasets = ['mitbih', 'ptbdb']
+available_datasets = ['mitbih', 'ptbdb', 'hapt']
 
 parameters = {}
 
 
 # setting the parameters
 def set_parameters(
-        path_to_database='./data/',
-        database_name='mitbih',
+        path_to_database='/mnt/Data/Database/TimeNetData/',
+        database_name='hapt',
 ):
     parameters['path_to_database'] = path_to_database
     parameters['database_name'] = database_name
@@ -27,6 +27,9 @@ if __name__ == '__main__':
     # importing the required libraries
     if parameters['database_name'] == 'mitbih':
         import library.dataset_importers.importer_mitbih as database_importer
+    if parameters['database_name'] == 'hapt':
+        import library.dataset_importers.importer_hapt as database_importer
+     
 
     # retrieving the data from cloud and storing it in the specified data folder:
     database_importer.download_the_original(parameters) #phase 1
